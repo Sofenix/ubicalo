2425010fc2de8f0501c15ceed009fece4e0068ca
maironsoft <maironsoft@gmail.com>
2016-06-11 11:08:25 -0500

    Corrección de librerias

--- www/js/index.js
+++ www/js/index.js
@@ -53,26 +53,12 @@ $(document).ready(function(){
     var ua = $.browser; /* Create a variable for browser info */
     $('.contenedor, #header').hide(); /* Hide the content */
 
-    /* If the Version or Firefox is < or = 5.0 the preloader not display, 
-        the same function applies to old Version Safari and Chrome
-        and finally Opera and IE they don't support CSS3 Animation */
-    if (ua.mozilla && ua.version <='5.0' || ua.safari && (navigator.appVersion.indexOf('3.') != -1) || ua.opera || ua.msie){
-        /* If the condition is true the content will be displayed immediately */
-        $('.contenedor').show();
-    }
-    else {  /* Otherwise appears the preloader */
-        /* Insert the markup preloader usign jQuery */
-        //$('body').append('<div id="logo-preload"><img scr="img/logo.png"></div><div class="loader" id="loader">Loading...</div>');
-        /* The animation during 2sec, 
-                change the delay parameter to extend or decrase the animation, 
-                remember to change the duration of the animation also in CSS */
-        $('#loader, #logo-preload').delay(2000).fadeOut('slow', function() {
-            $('.contenedor, #header').fadeIn('fast');  
-        });     
-    }  
+    $('#logo-preload').delay(2000).fadeOut('slow', function() {
+        $('.contenedor, #header').fadeIn('fast');  
+    });  
 
     $("#ventana").load(function (){
-        checkConnection()
+        //checkConnection()
     });
 
 });
